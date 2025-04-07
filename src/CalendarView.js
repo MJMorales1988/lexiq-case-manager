6a0d3b279801d6c1a9b5d731df33e655f1aaedf2
MJMorales1988 <119358290+MJMorales1988@users.noreply.github.com>
2025-04-07 11:16:46 +0800

    Calendar View update 3

--- src/CalendarView.js
+++ src/CalendarView.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
