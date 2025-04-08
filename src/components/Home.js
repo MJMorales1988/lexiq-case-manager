3e3e5fb4471bf63a559329c0f1c09a8b622d8308
MJMorales1988 <119358290+MJMorales1988@users.noreply.github.com>
2025-04-08 09:34:21 +0800

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to LexiQ Case Manager</h1>
+    </div>
+  );
+}
+
+export default Home; // Make sure this is the default export
