94030aa9e9704fc5973082a07374a0bd249dfcd9
MJMorales1988 <119358290+MJMorales1988@users.noreply.github.com>
2025-04-08 09:46:55 +0800

    Create Settings.js

---
+++ src/views/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>Configure your application settings here.</p>
+    </div>
+  );
+}
+
+export default Settings;
